9e0fb2ae21b4604b12f0241a4be18eb3f148b63c
Prim1Tive <46073419+Prim1Tive@users.noreply.github.com>
2021-01-24 02:58:52 +0200

    Create simplefileserver.py

---
+++ plonter/simplefileserver.py
@@ -0,0 +1,18 @@
+import http.server
+import socketserver
+import socket
+
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+PORT=8000
+IP = str(get_ip_address())
+print(f'{IP}:{PORT}')
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
+
